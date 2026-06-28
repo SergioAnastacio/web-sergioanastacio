6db61e9ec10c0d0f86a5aee955953996fdad1c3a
Sergio Anastacio Angulo Rosales <173404346+SergioAnastacio@users.noreply.github.com>
2026-06-27 21:46:23 -0600

    feat: add react tailwind visual island

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,14 @@
 import { defineConfig } from 'astro/config';
 import sitemap from '@astrojs/sitemap';
 
+import react from '@astrojs/react';
+import tailwindcss from '@tailwindcss/vite';
+
 export default defineConfig({
   site: 'https://sergioanastacio.com',
-  integrations: [sitemap()],
+  integrations: [sitemap(), react()],
+
+  vite: {
+    plugins: [tailwindcss()],
+  },
 });
